a885d1cdfa0d470debacefdb35c8f269f326cd2d
Jonas Eklöf <jonas_eklof89@hotmail.com>
2025-01-22 14:35:08 +0100

    --

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-react-app",
   server: {
     hmr: true, // Aktivera HMR explicit
     watch: {
