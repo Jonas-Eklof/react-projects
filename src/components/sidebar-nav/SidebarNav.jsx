f33468909878de10121761fdb8fa871504ab2453
Jonas Eklöf <jonas_eklof89@hotmail.com>
2025-01-22 16:18:43 +0100

    --

--- src/components/sidebar-nav/SidebarNav.jsx
+++ src/components/sidebar-nav/SidebarNav.jsx
@@ -5,46 +5,46 @@ import "./SidebarNav.css";
 export default function SidebarNav() {
   return (
     <ul className="sidebar-nav">
-      <Link to="/react-projects/">
+      <Link to="/">
         <li>Profile Card</li>
       </Link>
-      <Link to="/react-projects/ParentProfileCard">
+      <Link to="/ParentProfileCard">
         <li>Parent Profile Card</li>
       </Link>
-      <Link to="/react-projects/Ancestree">
+      <Link to="/Ancestree">
         <li>Ancestree</li>
       </Link>
-      <Link to="/react-projects/Counter">
+      <Link to="/Counter">
         <li>Counter</li>
       </Link>
-      <Link to="/react-projects/Timer">
+      <Link to="/Timer">
         <li>Timer</li>
       </Link>
-      <Link to="/react-projects/Greeting">
+      <Link to="/Greeting">
         <li>Greeting</li>
       </Link>
-      <Link to="/react-projects/InputDisplay">
+      <Link to="/InputDisplay">
         <li>Input Display</li>
       </Link>
-      <Link to="/react-projects/ButtonWithState">
+      <Link to="/ButtonWithState">
         <li>Button with state</li>
       </Link>
-      <Link to="/react-projects/MemoryGame">
+      <Link to="/MemoryGame">
         <li>Memory Game</li>
       </Link>
-      <Link to="/react-projects/ToggleVisible">
+      <Link to="/ToggleVisible">
         <li>Toggle visible</li>
       </Link>
-      <Link to="/react-projects/Hackathon">
+      <Link to="/Hackathon">
         <li>"Hackathon"</li>
       </Link>
-      <Link to="/react-projects/ClassButtons">
+      <Link to="/ClassButtons">
         <li>ClassButtons</li>
       </Link>
-      <Link to="/react-projects/PropStyleButton">
+      <Link to="/PropStyleButton">
         <li>PropStyleButton</li>
       </Link>
-      <Link to="/react-projects/ToDo">
+      <Link to="/ToDo">
         <li>ToDo-list</li>
       </Link>
     </ul>
